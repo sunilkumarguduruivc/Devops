ef1a69498c93cb6073cb5358e60f71129a8d352c
Guduru Namratha <sunilkumarguduruivc@gmail.com>
2026-01-07 02:40:46 +0530

    special Variables

--- 03SpecialVariables.sh
+++ 03SpecialVariables.sh
@@ -6,5 +6,5 @@ echo "Script name: $0"
 echo "Present working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is executing the script: $USER"
-echo "Process ID of the current script: $$
-ehco "process ID of the last executed command: $!"
+echo "process id of the current script: $$"
+echo "process id of the last executed command: $!"

d6e489c08ff3d5887bd26f4ea69d667598740d4d
Guduru Namratha <sunilkumarguduruivc@gmail.com>
2026-01-08 01:00:23 +0530

    app installation script

--- 06Logs.sh
+++ 06Logs.sh
@@ -4,7 +4,7 @@
 USERID=$(id -u) 
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_File=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 

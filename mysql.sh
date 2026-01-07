f1e449c164ca96b83ce9c18539672a1b5379b232
Guduru Namratha <sunilkumarguduruivc@gmail.com>
2026-01-08 02:01:31 +0530

    mysql setup

---
+++ mysql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+USERID=$(id -u) 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR: $2 installation failed."
+        exit 1
+    else
+        echo "$2 installed successfully."
+    fi
+}
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]; then
+    echo "ERROR: You must have root privileges to run this script."
+    exit 1
+fi
+}
+echo "Script started execution at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf list installed mysql-server &>>$LOG_FILE_NAME
+VALIDATE $? "Installed MYSQL SERVER"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling MYSQL Server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting MYSQL Server"
+
+mysql_secure_installation  --set-root-pass 'ExpenseApp@1'
+VALIDATE $? "Setting root password"
+
+
+

04c961610bfb08e963d2e1891595c1f851aded4c
Guduru <sunilkumar.guduru@thomsonreuters.com>
2026-01-12 03:37:12 +0530

    new changes

---
+++ expense-shell/backend.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+USERID=$(id -u) 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR: $2 failed."
+        exit 1
+    else
+        echo "$2  successfully."
+    fi
+}
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]; then
+    echo "ERROR: You must have root privileges to run this script."
+    exit 1
+fi
+}
+echo "Script started execution at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>LOG_FILE_NAME
+VALIDATE $? "nodejs disabled" 
+
+dnf module enable node:js -y &>>LOG_FILE_NAME
+VALIDATE $? "nodjs:20 enabled" 
+
+dnf install nodjs -y &>>LOG_FILE_NAME
+VALIDATE $? "nodejs installed"
+
+id expense
+if [ ?$ -ne 0 ] ; then
+    adduser expense &>>LOG_FILE_NAME
+    VALIDATE $? "expense user created"
+else 
+    echo -e "user already created"
+fi
+
+mkdir -p /app &>>LOG_FILE_NAME
+VALIDATE $? "Creating /app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading backend"
+
+cd /app
+rm -rf /app/*
+
+unzip /tmp/backend.zip &>>LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+dnf install mysql -y &>>LOG_FILE_NAME
+VALIDATE $? "mysql installed"
+
+PRIVATE_IP=$(hostname -I | awk '{print $1}')
+
+mysql -h "$PRIVATE_IP" -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Enabled"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting backend"
+
+
